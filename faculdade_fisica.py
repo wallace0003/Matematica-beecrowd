d35141ccd7f499492e6e8e74359c66547a9a3ffa
wallace0003 <wallaceizidoro.1@gmail.com>
2024-07-05 11:39:01 -0300

    falculdade física feito

---
+++ faculdade_fisica.py
@@ -0,0 +1,25 @@
+'''
+Uma partícula tem velocidade inicial e aceleração constante. Se a sua velocidade após certo
+ momento é v então qual será seu deslocamento no dobro deste tempo?
+'''
+
+import sys
+
+def main():
+    sys.stdin.read
+    data = input().split()
+    
+    i = 0
+    results = []
+    while i < len(data):
+        v = int(data[i])
+        t = int(data[i+1])
+        i += 2
+        s = 2 * v * t
+        results.append(s)
+    
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
